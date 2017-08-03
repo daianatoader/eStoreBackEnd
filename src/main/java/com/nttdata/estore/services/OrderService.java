c0066445068ac8de006637e4112112151ca98708
Carina <carina_matei@yahoo.com>
2017-08-03 11:39:32 +0300

    OrderController,Order,OrderRepository,OrderService ProductController,Product,ProductRepository,ProductService

---
+++ src/main/java/com/nttdata/estore/services/OrderService.java
@@ -0,0 +1,17 @@
+package com.nttdata.estore.services;
+
+import com.nttdata.estore.entities.Order;
+import com.nttdata.estore.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Iterable<Order> findAll() {
+        return orderRepository.findAll();
+    }
+}
