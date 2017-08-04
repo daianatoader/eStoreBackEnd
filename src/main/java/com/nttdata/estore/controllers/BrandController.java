f301547ce4b2613942cc8ce8ffebf54621a16ace
Daiana Toader <toaderdaiana95@yahoo.com>
2017-08-04 10:31:58 +0300

    Product and Brand updated

--- src/main/java/com/nttdata/estore/controllers/BrandController.java
+++ src/main/java/com/nttdata/estore/controllers/BrandController.java
@@ -1,15 +1,12 @@
 package com.nttdata.estore.controllers;
+
 import com.nttdata.estore.entities.Brand;
-import com.nttdata.estore.repositories.BrandRepository;
 import com.nttdata.estore.services.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(path="/brand")
