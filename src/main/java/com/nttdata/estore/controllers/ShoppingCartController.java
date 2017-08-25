94a31c79e9acf49494987f631ba69d6ea7b425b1
ioanmarcu <ioan.marcu@nttdata.ro>
2017-08-25 07:45:52 +0300

    update order price

--- src/main/java/com/nttdata/estore/controllers/ShoppingCartController.java
+++ src/main/java/com/nttdata/estore/controllers/ShoppingCartController.java
@@ -2,15 +2,13 @@ package com.nttdata.estore.controllers;
 
 import com.nttdata.estore.entities.Client;
 import com.nttdata.estore.entities.Order;
-import com.nttdata.estore.entities.OrderStatus;
 import com.nttdata.estore.services.ClientService;
 import com.nttdata.estore.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @CrossOrigin
@@ -23,16 +21,18 @@ public class ShoppingCartController {
     private OrderService orderService;
 
     @GetMapping(path = "/cart")
-    public @ResponseBody
-    Iterable<Order> getAllOrders() {
+    public @ResponseBody Iterable<Order> getAllOrders() {
         return orderService.findAll();
     }
 
     @GetMapping(path = "/cart/{id}")
+    @Transactional
     public ResponseEntity getOrder(@PathVariable("id") int id) {
         Client client = clientService.getClient(1);
         Order order = orderService.getOpenOrderForClient(client);
-        System.out.println(order);
+        if (order != null) {
+            order.updatePrice();
+        }
         return new ResponseEntity(order, HttpStatus.OK);
     }
 }
